c5dd7faaf39fa640cf333f6e4ea5843a8d3fee9b
Maylon-tech <minatokiroisen77@gmail.com>
2022-02-16 13:08:31 +0900

    estrutura JS inicio

---
+++ main.js
@@ -0,0 +1,25 @@
+
+/*const Main --> Objeto Principal da Aplicação - controle único e específico*/
+const Main = {
+
+    init: function() {
+
+    },
+
+
+    cacheSelectors: function() {
+
+    },
+
+
+    bindEvents: function() {
+        console.log('Belza')
+    }
+
+}
+
+Main.init()
+
+Main.cacheSelectors()
+
+Main.bindEvents()
