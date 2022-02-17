68385e3e8cf6fcd3dd460a724f2d51d68d0a638d
Maylon-tech <minatokiroisen77@gmail.com>
2022-02-17 21:58:36 +0900

    Js-recovered

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
 
-/*const Main --> Objeto Principal da Aplicação - controle único e específico*/
 const Main = {
-
+    /*THIS --> informa que o cacheSelectors está dentro do main.. - que está no pai..)com contextos diferentes no JS(*/
     init: function() {
-
+        this.cacheSelectors()
+        this.bindEvents()
     },
 
 
-    cacheSelectors: function() {
 
+    cacheSelectors: function() { /*Responsáveis por criar as variáveis*/
+       this.checkButtons = document.querySelectorAll('.check')
     },
-
 
     bindEvents: function() {
-        console.log('Belza')
-    }
+        const self = this
+        this.checkButtons.forEach(function(button){
+            button.onclick = self.Events.checkButton_click
+            
+        })
+    },
 
+
+    /*Funções relacionadas a Eventos*/
+    Events: {
+        checkButton_click: function(e) {
+            const li = e.target.parentElement
+            const isDone = li.classList.contains('done')
+            
+            if(isDone) {
+                li.classList.remove('done')
+            } else {
+                li.classList.add('done')
+            }
+            
+            /*  BOA PRÁTICA DE JS-SENTENÇA LÓGICA
+
+            if(isDone) {
+                return li.classList.remove('done')
+            }  
+
+            li.classList.add('done')
+            
+            */
+
+        }
+    }
 }
 
 Main.init()
-
-Main.cacheSelectors()
-
-Main.bindEvents()
